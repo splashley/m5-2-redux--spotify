f8cbfed88207a493c61acce60d87a843a29b470b
splashley <ashleyjlhynes@gmail.com>
2020-10-05 18:48:19 -0400

    Exercise 5: Routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+import GlobalStyles from "./GlobalStyles";
+import ArtistRoute from "./ArtistRoute";
+
+const DEFAULT_ARTIST_ID = "5z1VAFwT35EVvCp1XlZZuL";
 
 const App = () => {
-  return <Router>TODO</Router>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to={`/artists/${DEFAULT_ARTIST_ID}`} />
+        </Route>
+        <Route path="/artists/:id">
+          <ArtistRoute />
+        </Route>
+      </Switch>
+      <GlobalStyles />
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/App/ArtistRoute.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArtistRoute = () => {
+  return <div>Artist: Diana Krall</div>;
+};
+
+export default ArtistRoute;
